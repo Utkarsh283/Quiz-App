b6f447b1d60d2f0f9d1f8641d49d5a41d99543ac
Utkarsh283 <patelutkarsh283@gmail.com>
2024-11-28 16:57:03 +0530

    First Commit

---
+++ src/components/MasterComponent.jsx
@@ -0,0 +1,17 @@
+import Button from './Button';
+import ErrorBox from './ErrorBox';
+import Option from './Option';
+import ProgressBar from './ProgressBar';
+import QuizLink from './QuizLink';
+import QuizCompletedComponent from './QuizCompletedComponent';
+import Loader from './Loader';
+
+export {
+  Button,
+  ErrorBox,
+  Option,
+  ProgressBar,
+  QuizLink,
+  QuizCompletedComponent,
+  Loader,
+};
